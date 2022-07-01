707082c0aa13f5625402d58d2c82863b17ffcd62
ANU <anunallatt02@gmail.com>
2022-07-01 12:28:00 +0530

    1st python class

---
+++ Day1(1).py
@@ -0,0 +1,49 @@
+# print("Hello World")
+
+
+# Arithetic Operation
+# a=10
+# b=20
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a/b
+# g=a%b
+# h=a//b
+# print("Addition:",c)
+# print("Subtraction:",d)
+# print("Multiplication:",e)
+# print("Division:",f)
+# print("Modular division:",g)
+# print("Floor division:",h)
+
+
+# Logical operation
+# a=True
+# b=False
+# c = a&b
+# print(c)
+# d=a|b
+# print(d)
+
+
+# a=10
+# b=20
+# y=(a*a)+(2*a*b)+(b*b)
+# print(y)
+
+
+# a=int(input("Enter a number:"))
+# b=int(input("Enter another number:"))
+# c=a+b
+# print(c)
+
+
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+if a < b:
+    print("a is less than b")
+elif b < a:
+    print("a is greater than b")
+print("finished")
+
---
+++ Day1(3).py
@@ -0,0 +1,28 @@
+
+# Largest of three numbers
+
+# a=int(input("Enter first number:"))
+# b=int(input("Enter sec number:"))
+# c=int(input("Enter third number:"))
+# if a > b:
+#     if a > c:
+#         print("a is the largest number")
+#     else:
+#         print("c is the largest number")
+# else:
+#     if b > c:
+#         print("b is the largest number")
+#     else:
+#         print("c is the largest number")
+
+
+
+a=int(input("Enter first number:"))
+b=int(input("Enter sec number:"))
+c=int(input("Enter third number:"))
+if a > b and a > c:
+    print("a is the largest number")
+elif b > c:
+    print("b is the largest number")
+else:
+    print("c is the largest number")
